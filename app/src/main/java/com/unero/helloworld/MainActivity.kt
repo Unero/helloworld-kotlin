1563cb23890a5c1c109770bc8ed10d95b8d77a8b
Unero <ramadhanbhg@gmail.com>
2020-09-08 10:29:26 +0700

    Adding Log

--- app/src/main/java/com/unero/helloworld/MainActivity.kt
+++ app/src/main/java/com/unero/helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package com.unero.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MyActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "Hello World, MyActivity")
     }
 }
